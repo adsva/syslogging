e16489923aeaf583f6f65ac794372c4761e588b9
Adam Svanberg <asvanberg@gmail.com>
2012-06-11 00:23:35 +0200

    Initial commit - for real this time

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='syslogging',
+      version="1.0",
+      py_modules=['syslogging'],
+      description="Import for decent default logging to syslog. Exceptions included.",
+      author="Adam Svanberg",
+      author_email="asvanberg@gmail.com")
---
+++ syslogging.py
@@ -0,0 +1,44 @@
+import sys
+import logging
+import traceback
+from logging.config import dictConfig
+
+syslog_cfg = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': 'python %(name)s %(levelname)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'syslog': {
+            'level':'INFO',
+            'class':'logging.handlers.SysLogHandler',
+            'formatter':'simple',
+            'address':'/dev/log',
+        },
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        '':{
+            'handlers': ['syslog','console'],
+            'level': 'INFO',
+        },
+    }
+}
+
+dictConfig(syslog_cfg)
+
+logger = logging.getLogger('exception')
+
+def log_exception(cls, val, tb, logger=logger):
+    exception = traceback.format_exception(cls, val, tb)
+    logger.error(val)
+    sys.__excepthook__(cls, val, tb)
+
+sys.excepthook = log_exception
