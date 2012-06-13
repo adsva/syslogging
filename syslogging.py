2ab2cef951d41b9743032da65ff0c39137366072
Adam Svanberg <asvanberg@gmail.com>
2012-06-13 20:05:29 +0200

    Proxy the logging module to avoid importing both to get loggers, etc

--- syslogging.py
+++ syslogging.py
@@ -1,7 +1,7 @@
 import sys
-import logging
 import traceback
 from logging.config import dictConfig
+from logging import *
 
 syslog_cfg = {
     'version': 1,
@@ -34,9 +34,9 @@ syslog_cfg = {
 
 dictConfig(syslog_cfg)
 
-logger = logging.getLogger('exception')
+exception_logger = getLogger('exception')
 
-def log_exception(cls, val, tb, logger=logger):
+def log_exception(cls, val, tb, logger=exception_logger):
     logger.error('Exception:')
     exc = ''.join(traceback.format_exception(cls, val, tb))
     for line in exc.rstrip().split('\n'):
